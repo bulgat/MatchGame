256462ffc2821988de0f1fa27387acb6ad174083
Nikolay K <sirbulgat@gmail.com>
2025-04-28 22:09:43 +0300

    Get zapros

---
+++ MatchGame/Controllers/GameController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MatchGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MatchGame.Controllers
+{
+    public class GameController : Controller
+    {
+
+        
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult GetNumber()
+        {
+
+            return Json(" ок");
+        }
+
+
+        }
+}
